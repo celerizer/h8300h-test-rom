d24a5a0dc2adfa5d6b7d7c9cdd987fe7ff51fca9
celerizer <33245078+celerizer@users.noreply.github.com>
2025-09-16 19:14:19 -0500

    add some more registers

--- src/sys.h
+++ src/sys.h
@@ -15,6 +15,7 @@
 #define H8_SSCR H8_REG16(0xf0e3)
 #define H8_SSCRH H8_REG8(0xf0e3)
 #define H8_SSCRL H8_REG8(0xf0e4)
+#define H8_SSRDR H8_REG8(0xf0e9)
 #define H8_SSRR H8_REG8(0xf0eb)
 
 #define H8_ADRR H8_REG16(0xffbc)
@@ -23,6 +24,8 @@
 #define H8_AMR H8_REG8(0xffbe)
 #define H8_ADSR H8_REG8(0xffbf)
 
+#define H8_PDR1 H8_REG8(0xffd4)
+#define H8_PDR3 H8_REG8(0xffd6)
 #define H8_PDR8 H8_REG8(0xffdb)
 #define H8_PDR9 H8_REG8(0xffdc)
 #define H8_PDRB H8_REG8(0xffde)
